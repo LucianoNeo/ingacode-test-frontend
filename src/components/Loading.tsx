8fcb67fb2ada26785f1701faa3458cef08761176
LUCIANO DOS SANTOS BUENO <tec.info.luciano@hotmail.com>
2023-01-13 16:12:45 -0300

    loading animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,3 +1,4 @@
+import { ScaleLoader } from "react-spinners";
 import { useMyContext } from "../contexts/MyContext";
 
 export default function Loading() {
@@ -6,7 +7,10 @@ export default function Loading() {
 
     return (
         <div className={`${!isLoading && 'hidden'} w-screen h-screen bg-black bg-opacity-80 backdrop:blur-3xl flex items-center justify-center z-[55] absolute top-0 left-0`}>
-            <div>CARREGANDO ...</div>
+            <div className="flex flex-col items-center justify-center">
+                <ScaleLoader loading width={26} height={36} color="rgb(234 88 12)" />
+                <h1>CARREGANDO ...</h1>
+            </div>
         </div>
     );
 }
