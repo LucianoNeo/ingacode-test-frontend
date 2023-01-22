25e78417a51e61fc53fb0d48d6ff45f5650be175
LUCIANO DOS SANTOS BUENO <tec.info.luciano@hotmail.com>
2023-01-22 09:27:58 -0300

    new backend address

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://neotasks-backend.up.railway.app'
+    //baseURL: 'https://neotasks-backend.up.railway.app'
     //baseURL: 'http://localhost:3333'
+    baseURL: 'http://154.12.232.78:3333'
 })
 
